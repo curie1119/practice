5250c6a77b4dc13b91c72dcf1d0300b4730c0e29
Zelenkov Ivan <zelenkov_vanya@mail.ru>
2023-07-07 19:43:05 +0700

    Fourth commit: full tasks of block 1 and 2

---
+++ Pt_2_1.py
@@ -0,0 +1,21 @@
+import  random
+print ("фиолетовый, красный, синий, оранжевый, зелёный")
+colour = random.choice(["фиолетовый", "красный", "синий", "оранжевый", "зелёный"])
+again = True
+while again == True:
+    choice = input("Выберите цвет из вышеперечисленных:  ")
+    choice = choice.lower()
+    if colour == choice:
+        print("Отлично!")
+        again = False
+    else:
+        if colour == "фиолетовый":
+            print("Цвет баклажана?")
+        elif colour == "красный":
+            print("Цвет помидора?")
+        elif colour == "синий":
+            print("Цвет голубики?")
+        elif colour == "оранжевый":
+            print("Цвет апелсина?")
+        else:
+            print("Цвет огурца?")
