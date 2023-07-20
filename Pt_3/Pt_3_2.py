fb2bc1544f8c8a0cba0ad1abaf516b4b9f4a57ba
Zelenkov Ivan <zelenkov_vanya@mail.ru>
2023-07-20 23:48:55 +0700

    Commit 19: Pep 8 change

--- Pt_3/Pt_3_2.py
+++ Pt_3/Pt_3_2.py
@@ -1,4 +1,4 @@
 from functools import reduce
 a = int(input("Введите количество чисел: "))
-fib = lambda n: reduce(lambda x, _: x+[x[-1]+x[-2]], range(n-2), [0, 1])
+fib = lambda n: reduce(lambda x, _: x + [x[-1] + x[-2]], range(n - 2), [0, 1])
 print(fib(a))
